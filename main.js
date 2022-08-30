9760775cfe4506754223a20adb5b4d58eb098883
Elvis <s49188705@gmail.com>
2022-08-30 23:06:19 +0300

    Css/Html/Main.js modified

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const popUp = document.getElementById('popup')
+
+function openPopUp(){
+    popup.classList.add('open-popup');
+}
+
+function closePopUp(){
+    popUp.classList.remove('open-popup');
+}
+
+
+
+
+
+
 fetch('https://api.openbrewerydb.org/breweries/random')
 .then(resp => resp.json())
 .then(data => console.log(data))
