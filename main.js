214cd7368ec9ddb1df3168d520a42db30ca607f4
Elvis <s49188705@gmail.com>
2022-08-31 14:36:28 +0300

    Html/Css/images Modifications

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-const popUp = document.getElementById('popup')
+//Grabbing Elements
+const breweriesAPI = "https://api.openbrewerydb.org/breweries/random";
+const popUpDisplay = document.getElementById("popup");
+const searchButton = document.getElementById("submit");
+const exitButton = document.getElementById("exit");
 
-function openPopUp(){
-    popup.classList.add('open-popup');
+//fetching API
+fetch(breweriesAPI)
+  .then((resp) => resp.json())
+  .then(IterateData);
+
+//Iterating through each element in the Array
+function IterateData(data) {
+  data.forEach(addDetails);
 }
 
-function closePopUp(){
-    popUp.classList.remove('open-popup');
+//Assigning Elements from the array
+function addDetails(breweryarray) {
+  popUpDisplay.Name = breweryarray.name;
+  popUpDisplay.Brewerytype = breweryarray.brewery_type;
+  popUpDisplay.City = breweryarray.city;
+  popUpDisplay.Country = breweryarray.country;
+  popUpDisplay.BreweryID = breweryarray.id;
+  popUpDisplay.phoneNumber = breweryarray.phone;
+  popUpDisplay.PostalCode = breweryarray.postal_code;
+  popUpDisplay.StateName = breweryarray.state;
+  popUpDisplay.StreetName = breweryarray.street;
+  popUpDisplay.BreweryWebsite = breweryarray.website_url;
 }
 
 
 
-
-
-
-fetch('https://api.openbrewerydb.org/breweries/random')
-.then(resp => resp.json())
-.then(data => console.log(data))
+// Pop-up Display Function
+function openPopUp() {
+  popup.classList.add("open-popup");
+}
+function closePopUp() {
+  popup.classList.remove("open-popup");
+}
